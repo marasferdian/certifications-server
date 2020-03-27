5ee549591ddb9228ab9d7c52ab09f9c6b32d2e0c
Mara Sferdian <marasferdian@gmail.com>
2020-03-27 21:17:39 +0200

    Create project structure

---
+++ src/main/java/com/ibm/certificationsserver/CertificationsServerApplication.java
@@ -0,0 +1,13 @@
+package com.ibm.certificationsserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CertificationsServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CertificationsServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ibm/certificationsserver/CertificationsServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibm.certificationsserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CertificationsServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
